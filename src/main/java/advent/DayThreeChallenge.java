a74913951c3071c75483cf2e857770b1025db535
Bartoszko <bartek.zawodniak@gmail.com>
2024-01-28 03:42:28 +0100

    First part of third challenge completed

--- src/main/java/advent/DayThreeChallenge.java
+++ src/main/java/advent/DayThreeChallenge.java
@@ -15,69 +15,112 @@ import lombok.Setter;
 public class DayThreeChallenge {
 
 	public long solveChallenge(List<String> lines) {	
-		Map<Integer, List<Element>> linesOfElements = parseElements(lines);
+		Map<Integer, List<Element>> linesOfElements = parseLinesToMapOfElements(lines);
 		List<Element> engineParts = filterEngineParts(linesOfElements);
 		return sumEngineParts(engineParts);
 	}
-	
-//	protected long sumEngineParts(Map<Integer, List<Element>> engineParts) {
-//		int sum = 0;
-//	for (Entry<Integer, List<Element>> elements : engineParts.entrySet()) {
-//		sum += elements.getValue().stream().map(e -> e.element).mapToInt(s -> Integer.valueOf(s)).sum();
-//	}
-//		return sum;
-//	}
 
+	protected Map<Integer, List<Element>> parseLinesToMapOfElements(List<String> lines) {
+		Map<Integer, List<Element>> elements = new HashMap<>();
+		for (String line : lines) {		
+			elements.put(elements.size(), processLine(line));
+		}
+		return elements;	
+	}
+
+	List<Element> processLine (String line) {
+		List<Element> elementsInLine = new ArrayList<>();
+		String tempPart = "";
+		int pos = 1;
+		for (char elementChar : line.toCharArray()) {
+			String elementString = Character.toString(elementChar);
+			if (elementString.matches("[0-9]")) {
+				tempPart += elementString;
+				pos++;
+				continue;
+			} else { 
+				if (!tempPart.isEmpty()) {
+					Element enginePart = createElement(tempPart, true, pos - tempPart.length(), pos - 1);
+					tempPart = "";
+					elementsInLine.add(enginePart);
+				}
+				if (!elementString.equals(".")) {
+					Element symbol = createElement(elementString, false, pos, pos);	
+					elementsInLine.add(symbol);
+				}
+				pos++;
+			}
+		}
+		if (!tempPart.isEmpty()) {
+			Element enginePart = createElement(tempPart, true, pos - tempPart.length(), pos - 1);
+			elementsInLine.add(enginePart);
+		}
+		return elementsInLine;
+	}
+
+	protected List<Element> filterEngineParts(Map<Integer, List<Element>> linesOfElements) {
+		List<Element> listOfParts = new ArrayList<>();
+		TestLines testLines = new TestLines(Collections.emptyList(), linesOfElements.get(0), linesOfElements.get(1));
+		for (Entry<Integer, List<Element>> lineOfElements : linesOfElements.entrySet()) {
+			List<Element> line = lineOfElements.getValue();
+			testLines.addNextLine(line);
+			List<Element> engineParts = getEngineParts(testLines);
+			listOfParts.addAll(engineParts);
+		}		
+		testLines.addNextLine(Collections.emptyList());
+		List<Element> engineParts = getEngineParts(testLines); 
+		listOfParts.addAll(engineParts);
+		return listOfParts;
+	}
+
+	protected List<Element> getEngineParts (TestLines testLines) {
+		List<Element> engineParts = new ArrayList<>();
+		List<Element> currentLine = testLines.getCurrentLine();
+		if (currentLine == null || currentLine.isEmpty()) {
+			return Collections.emptyList();
+		}
+		for (Element element : currentLine) {
+			if (element.isEnginePart()) {
+				int startIndex = element.getStartIndex();
+				int endIndex = element.getEndIndex();
+				boolean hasAdjacentElement = checkLines(startIndex, endIndex, testLines);
+				if (hasAdjacentElement) {
+					engineParts.add(element);
+				}	
+			}
+		}
+		return engineParts;
+	}
+	
+	protected boolean checkLines(int startIndex, int endIndex, TestLines testLines) {
+		boolean adjacentSymbolPrev = checkLine(startIndex, endIndex, testLines.getPreviousLine());
+		boolean adjacentSymbolCurr = checkLine(startIndex, endIndex, testLines.getCurrentLine());
+		boolean adjacentSymbolNext = checkLine(startIndex, endIndex, testLines.getNextLine());
+		return adjacentSymbolPrev || adjacentSymbolCurr || adjacentSymbolNext;
+	}
+	
+	protected boolean checkLine(int startIndex, int endIndex, List<Element> line) {
+		if (line.isEmpty()) {
+			return false;
+		}
+		for (Element element : line) {
+			if (!element.isEnginePart) {
+				int symbolPos = element.getStartIndex();
+				for (int i = startIndex - 1 ; i <= endIndex + 1 ; i++ ) {
+					if (symbolPos == i) {
+						return true;
+					}
+				}
+			}	
+		}	
+		return false;
+	}
+	
 	protected long sumEngineParts(List<Element> engineParts) {
 	return engineParts.stream()
 			.map(Element::getElement)
 			.map(Integer::valueOf)
 			.reduce(0,Integer::sum);
-	}
-	
-	protected List<Element> getEngineParts (TestLines testLines) {
-		
-		return testLines.getCurrentLine();
-	}
-	
-	protected List<Element> filterEngineParts(Map<Integer, List<Element>> linesOfElements) {
-		List<Element> listOfParts = new ArrayList<>();
-		TestLines testLines = new TestLines();
-		
-		for (Entry<Integer, List<Element>> lineOfElements : linesOfElements.entrySet()) {
-			List<Element> line = lineOfElements.getValue();
-			testLines.addNextLine(line);
-			List<Element> engineParts = getEngineParts(testLines); 
-			listOfParts.addAll(engineParts);
-		}
-		return listOfParts;
-	}
-		
-	protected Map<Integer, List<Element>> parseElements(List<String> lines) {
-		Map<Integer, List<Element>> elements = new HashMap<>();
-		for (String line : lines) {		
-			elements.put(elements.size(), processLine(line));
-		}
-	return elements;	
-	}
-		
-	private List<Element> processLine (String line) {
-		List<Element> elementsInLine = new ArrayList<>();	
-		for (String elementString : line.split("\\.")) {
-			if (elementString.length() == 0) {
-				continue;
-			}
-			Element element = newElementFromString(line, elementString);
-			elementsInLine.add(element);
-		}
-		return elementsInLine;
-	}
-	
-	private Element newElementFromString (String line, String elementString) {
-		int startIndex = line.indexOf(elementString);
-		int endIndex = startIndex + elementString.length();
-		boolean isEnginePart = elementString.matches("[1-9]*");
-		return createElement(elementString, isEnginePart, startIndex, endIndex);
 	}
 	
 	protected Element createElement(String element, boolean isEnginePart, int startIndex, int endIndex) {
@@ -104,20 +147,20 @@ public class DayThreeChallenge {
 
 		List<Element> previousLine;
 		
-		List<Element> currentLine;
+		List<Element> currentLine; 
 		
 		List<Element> nextLine;
 		
 		public TestLines (List<Element> previousLine, List<Element> currentLine, List<Element> nextLine) {
-			previousLine = previousLine != null ? previousLine : Collections.emptyList();
-			currentLine = currentLine != null ? currentLine : Collections.emptyList();
-			nextLine = nextLine != null ? nextLine : Collections.emptyList();
+			this.previousLine = previousLine != null ? previousLine : Collections.emptyList();
+			this.currentLine = currentLine != null ? currentLine : Collections.emptyList();
+			this.nextLine = nextLine != null ? nextLine : Collections.emptyList();
 		}
 		
-		public void addNextLine(List<Element> newNextLine) {
-			previousLine = currentLine;
-			currentLine = nextLine;
-			nextLine = newNextLine != null ? newNextLine : Collections.emptyList();
+		public void addNextLine(List<Element> nextLine) {
+			this.previousLine = this.currentLine;
+			this.currentLine = this.nextLine;
+			this.nextLine = nextLine != null ? nextLine : Collections.emptyList();
 		}
 	}
 	
--- src/test/java/advent/DayThreeChallengeTest.java
+++ src/test/java/advent/DayThreeChallengeTest.java
@@ -2,7 +2,6 @@ package advent;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -17,25 +16,53 @@ public class DayThreeChallengeTest {
 
 	DayThreeChallenge challenge;
 	
-	List<String> testInput = Arrays.asList("467..114..","...*......","..35..633.","......#...","617*......",
-			".....+.58.","..592.....","......755.","...$.*....",".664.598..");
-
-	
+	List<String> testInput = Arrays.asList("467..114..",
+										   "...*......",
+										   "..35..633.",
+										   "......#...",
+										   "617*......",
+										   ".....+.58.",
+										   "..592.....",
+										   "......755.",
+										   "...$.*....",
+										   ".664.598..");
 	@Before
 	public void setUp() {
 		challenge = new DayThreeChallenge();
 	}
 	
-//	@Test
-//	public void testExample() {
-//		long result = challenge.solveChallenge(testInput);
-//		Assert.assertTrue(result == 4361);
-//	}	
+	@Test
+	public void testExample() {
+		long result = challenge.solveChallenge(testInput);
+		Assert.assertTrue(result == 4361);
+	}	
 
 	@Test
+	public void testProcessingLineWithEnginePart() {
+		List<Element> processedLine = challenge.processLine("..456..");
+		Element element = processedLine.get(0);
+		Assert.assertTrue(element.isEnginePart);
+		Assert.assertEquals(3, element.getStartIndex());
+		Assert.assertEquals(5, element.getEndIndex());
+		Assert.assertEquals("456", element.getElement());	
+	}
+	
+	@Test
+	public void testProcessingLineWithSymbol() {
+		List<Element> processedLine = challenge.processLine("..@..");
+		Element element = processedLine.get(0);
+		Assert.assertFalse(element.isEnginePart);
+		Assert.assertEquals(3, element.getStartIndex());
+		Assert.assertEquals(3, element.getEndIndex());
+		Assert.assertEquals("@", element.getElement());	
+	}
+	
+	@Test
 	public void testParsingLines() {
-		Map<Integer, List<Element>> parts = challenge.parseElements(Arrays.asList("..456....123....", ".12....158"));
-		Map<Integer, List<Element>> symbols = challenge.parseElements(Arrays.asList(".&....#....@"));
+		Map<Integer, List<Element>> parts = challenge.parseLinesToMapOfElements(Arrays.asList("..456....123....", ".12....158"));
+		Map<Integer, List<Element>> symbols = challenge.parseLinesToMapOfElements(Arrays.asList(".&....#....@"));
+		Map<Integer, List<Element>> mixed = challenge.parseLinesToMapOfElements(Arrays.asList(".&45....@"));
+		Map<Integer, List<Element>> doubles = challenge.parseLinesToMapOfElements(Arrays.asList(".45....45....@..@"));
 		Assert.assertTrue(parts.size() == 2);
 		Assert.assertTrue(parts.get(0).size() == 2 && parts.get(1).size() == 2 );
 		Assert.assertTrue(allEngineParts(parts.get(0)) && allEngineParts(parts.get(1)));		
@@ -43,6 +70,10 @@ public class DayThreeChallengeTest {
 		Assert.assertTrue(getValuesAsStrings(parts.get(1)).equals(Arrays.asList("12","158")));
 		Assert.assertTrue(symbols.get(0).size() == 3);
 		Assert.assertTrue(getValuesAsStrings(symbols.get(0)).equals(Arrays.asList("&","#","@")));
+		Assert.assertTrue(mixed.get(0).size() == 3);
+		Assert.assertTrue(getValuesAsStrings(mixed.get(0)).equals(Arrays.asList("&","45","@")));
+		Assert.assertTrue(doubles.get(0).size() == 4);
+		Assert.assertTrue(getValuesAsStrings(doubles.get(0)).equals(Arrays.asList("45","45","@","@")));
 	}
 	
 	private List<String> getValuesAsStrings (List<Element> elements) {
@@ -54,18 +85,34 @@ public class DayThreeChallengeTest {
 	}
 	
 	@Test
+	public void testCheckLine() {		
+		List<Element> line = new ArrayList<>();
+		Element firstSymbol = challenge.createElement("#", false, 1, 1);
+		Element secondSymbol = challenge.createElement("@", false, 10, 10);
+		Element thirdSymbol = challenge.createElement("&", false, 6, 6);
+		line.add(firstSymbol);
+		line.add(secondSymbol);
+		boolean adjacentSymbol = challenge.checkLine(3,5, line);
+		Assert.assertFalse(adjacentSymbol);
+
+		line.add(thirdSymbol);
+		boolean adjacentSymbol2 = challenge.checkLine(3,5, line);
+		Assert.assertTrue(adjacentSymbol2);
+		
+		boolean adjacentSymbol3 = challenge.checkLine(3,8, line);
+		Assert.assertTrue(adjacentSymbol3);
+	}
+		
+	@Test
 	public void testSumEngineParts() {
-		Element firstElement = challenge.createElement("456", false, 1, 4);
-		Element secondElement = challenge.createElement("1528", false, 1,54);
+		Element firstElement = challenge.createElement("456", true, 1, 4);
+		Element secondElement = challenge.createElement("1528", true, 2, 5);
 		List<Element> engineParts = new ArrayList<>();
 		engineParts.add(firstElement);
 		engineParts.add(secondElement);
 		long result = challenge.sumEngineParts(engineParts);
 		Assert.assertTrue(result == 1984);
 	}
-	
-	
-	
-	
+
 	
 }
